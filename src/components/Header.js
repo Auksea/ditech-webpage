455f265c79d5f90145c235569a41ed45e63f96f3
Auksea <66auksyte@gmail.com>
2024-03-01 12:21:19 +0100

    header fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import './Header.css';
 
 const Header = () => {
     const [animated, setAnimated] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Added state for dropdown toggle
     const location = useLocation();
     const isContactPage = location.pathname === '/contact';
     const isAboutPage = location.pathname === '/about';
@@ -27,6 +28,7 @@ const Header = () => {
     }
 
     const companyLogo = "/pics/Di-Tech-logo.jpg";
+
     return (
         <div className={`header-wrapper ${animated ? 'animated' : ''}`} style={{ backgroundImage: `url(${process.env.PUBLIC_URL}${headerImage})` }}>
             <div className={`heading-container ${isContactPage ? 'contact-page' : ''} ${isAboutPage ? 'about-page' : ''}`}>
@@ -37,8 +39,9 @@ const Header = () => {
                     <Link className="navbar-brand" to="/">
                         <img src={`${process.env.PUBLIC_URL}${companyLogo}`} alt="Company Icon" className="company-icon" />
                     </Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className={`navbar-toggler ${isDropdownOpen ? 'dropdown-open' : ''}`} type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
                         <span className="navbar-toggler-icon"></span>
+                        <span className="dropdown-line"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto">
@@ -49,15 +52,15 @@ const Header = () => {
                                 <Link className="nav-link" to="/about">About</Link>
                             </li>
                             <li>
-                                <Dropdown>
-                                    <Dropdown.Toggle id="dropdown-basic" className='nav-link'>
-                                    Services
+                                <Dropdown show={isDropdownOpen} onToggle={(isOpen) => setIsDropdownOpen(isOpen)}>
+                                    <Dropdown.Toggle id="dropdown-basic" className={`nav-link custom-dropdown-toggle ${isDropdownOpen ? 'dropdown-open' : ''}`}>
+                                        Services
                                     </Dropdown.Toggle>
                                     <Dropdown.Menu className="custom-dropdown-menu">
-                                    <Dropdown.Item as={Link} to="/maintenance-ecs">Maintenance of Engine Control System (ECS)</Dropdown.Item>
-                                    <Dropdown.Item as={Link} to="/troubleshooting">Troubleshooting</Dropdown.Item>
-                                    <Dropdown.Item as={Link} to="/performance">Performance optimization</Dropdown.Item>
-                                    <Dropdown.Item as={Link} to="/modification">Modification of cylinder lubricators (ME/Alpha)</Dropdown.Item>
+                                        <Dropdown.Item as={Link} to="/maintenance-ecs">Maintenance of Engine Control System</Dropdown.Item>
+                                        <Dropdown.Item as={Link} to="/troubleshooting">Troubleshooting</Dropdown.Item>
+                                        <Dropdown.Item as={Link} to="/performance">Performance optimization</Dropdown.Item>
+                                        <Dropdown.Item as={Link} to="/modification">Modification of cylinder lubricators</Dropdown.Item>
                                     </Dropdown.Menu>
                                 </Dropdown>
                             </li>
@@ -73,6 +76,10 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
+
 
 
 
